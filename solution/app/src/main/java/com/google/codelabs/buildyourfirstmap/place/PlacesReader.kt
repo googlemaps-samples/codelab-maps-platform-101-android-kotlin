9b737a4cccb8f32e02aefd9bb4c5cbac10a3f34c
Chris Arriola <carriola@google.com>
2020-04-17 11:21:02 -0700

    Comments.

--- solution/app/src/main/java/com/google/codelabs/buildyourfirstmap/place/PlacesReader.kt
+++ solution/app/src/main/java/com/google/codelabs/buildyourfirstmap/place/PlacesReader.kt
@@ -21,13 +21,22 @@ import com.google.gson.reflect.TypeToken
 import java.io.InputStream
 import java.io.InputStreamReader
 
+/**
+ * Reads a list of place JSON objects from the file places.json.
+ */
 class PlacesReader(private val context: Context) {
 
+    // GSON object responsible for converting from JSON to a Place object
     private val gson = Gson()
 
+    // InputStream representing places.json
     private val inputStream: InputStream
         get() = context.resources.openRawResource(R.raw.places)
 
+    /**
+     * Reads the list of place JSON objects in the file places.json and returns a list of Place
+     * objects
+     */
     fun read(): List<Place> {
         val itemType = object : TypeToken<List<PlaceResponse>>() {}.type
         val reader = InputStreamReader(inputStream)
