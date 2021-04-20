75ae8b730c8e76fc60e653a8bfc2805b5efb1b94
Chris Arriola <carriola@google.com>
2021-04-19 17:03:39 -0700

    Add solution for KTX.

---
+++ solution-ktx/app/src/main/java/com/google/codelabs/buildyourfirstmap/BitmapHelper.kt
@@ -0,0 +1,50 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.codelabs.buildyourfirstmap
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.util.Log
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+object BitmapHelper {
+    /**
+     * Demonstrates converting a [Drawable] to a [BitmapDescriptor], for use as a marker icon.
+     * Taken from ApiDemos on GitHub: https://github.com/googlemaps/android-samples/blob/master/ApiDemos/kotlin/app/src/main/java/com/example/kotlindemos/MarkerDemoActivity.kt
+     */
+    fun vectorToBitmap(context: Context, @DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
+        val vectorDrawable = ResourcesCompat.getDrawable(context.resources, id, null)
+        if (vectorDrawable == null) {
+            Log.e("BitmapHelper", "Resource not found")
+            return BitmapDescriptorFactory.defaultMarker()
+        }
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        DrawableCompat.setTint(vectorDrawable, color)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+}
---
+++ solution-ktx/app/src/main/java/com/google/codelabs/buildyourfirstmap/place/PlacesReader.kt
@@ -0,0 +1,47 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.codelabs.buildyourfirstmap.place
+
+import android.content.Context
+import com.google.codelabs.buildyourfirstmap.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.InputStream
+import java.io.InputStreamReader
+
+/**
+ * Reads a list of place JSON objects from the file places.json.
+ */
+class PlacesReader(private val context: Context) {
+
+    // GSON object responsible for converting from JSON to a Place object
+    private val gson = Gson()
+
+    // InputStream representing places.json
+    private val inputStream: InputStream
+        get() = context.resources.openRawResource(R.raw.places)
+
+    /**
+     * Reads the list of place JSON objects in the file places.json and returns a list of Place
+     * objects
+     */
+    fun read(): List<Place> {
+        val itemType = object : TypeToken<List<PlaceResponse>>() {}.type
+        val reader = InputStreamReader(inputStream)
+        return gson.fromJson<List<PlaceResponse>>(reader, itemType).map {
+            it.toPlace()
+        }
+    }
+}
